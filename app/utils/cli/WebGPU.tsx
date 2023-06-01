178b57f554fa7b4a537ebc724c17213a5ec25ab9
Junho Park <dwarfthema@gmail.com>
2023-06-01 12:19:41 +0900

    webGPU_Canvas size w,h-screen

--- app/utils/cli/WebGPU.tsx
+++ app/utils/cli/WebGPU.tsx
@@ -13,6 +13,7 @@ export function WebGPU(props: React.PropsWithChildren<any>) {
 
   return (
     <Canvas
+      className="w-screen h-screen"
       gl={(canvas) => {
         /* const r = new WebGPURenderer({ canvas });
         r.setClearColor(0x000000, 0);
