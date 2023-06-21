069b0b3ccf90ad1f5abb4990d4568e4b7ea86ac3
Junho Park <dwarfthema@gmail.com>
2023-06-21 19:32:14 +0900

    route_change file dir

--- app/projects/JeongeunPark/page.tsx
+++ app/projects/JeongeunPark/page.tsx
@@ -1,46 +1,69 @@
 "use client";
 
+import { Ground } from "@/app/utils/cli/Ground";
 import { Model } from "@/app/utils/cli/Model";
+import { Player } from "@/app/utils/cli/Player";
 import { WebGPU } from "@/app/utils/cli/WebGPU";
 
-import {
-  Bounds,
-  Environment,
-  OrbitControls,
-  PerspectiveCamera,
-  useGLTF,
-} from "@react-three/drei";
+import { Environment, KeyboardControls } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+import { Leva, useControls } from "leva";
 
-export default function Jeongeun1() {
+export default function JeongeunIndex() {
+  /*   const { objX, objY, objZ } = useControls({
+    objX: 0.5,
+    objY: 2,
+    objZ: 0,
+  }); */
+
   return (
     <main style={{ height: "100vh", width: "100%" }} className="bg-black">
-      {/*       <Canvas>
-        <OrbitControls makeDefault autoRotate />
-        <PerspectiveCamera position={[2, 1, 2]} makeDefault />
-
-        <Environment preset="city" />
-        <ambientLight intensity={0.5} />
-        <Bounds fit clip observe margin={1.3}>
-          <Thing />
-        </Bounds>
-      </Canvas> */}
-
       {
-        <WebGPU>
-          <OrbitControls makeDefault />
-          <PerspectiveCamera position={[2, 1, 2]} makeDefault near={0.001} />
-
-          <Environment files="/hdr/hansaplatz_2k.hdr" background />
-          <ambientLight intensity={0.5} />
-
-          <Bounds fit clip margin={3}>
-            <Model
-              meshDir="/glb/jeoungeun/JEOUNGEUN_join.gltf"
-              meshName="Group"
-              mtlName="Sculpture"
-            />
-          </Bounds>
-        </WebGPU>
+        <>
+          {/*  <Leva collapsed /> */}
+          <KeyboardControls
+            map={[
+              { name: "forward", keys: ["ArrowUp", "w", "W"] },
+              { name: "backward", keys: ["ArrowDown", "s", "S"] },
+              { name: "left", keys: ["ArrowLeft", "a", "A"] },
+              { name: "right", keys: ["ArrowRight", "d", "D"] },
+              { name: "jump", keys: ["Space"] },
+              { name: "sit", keys: ["c"] },
+            ]}
+          >
+            <WebGPU>
+              <Environment files="/hdr/hansaplatz_2k.hdr" background />
+              <ambientLight intensity={0.5} />
+              <Physics gravity={[0, -25, 0]}>
+                <Ground>
+                  <Model
+                    meshDir="/glb/jeoungeun/church/collider.gltf"
+                    meshName="collider"
+                    mtlName="collider_mtl"
+                  />
+                </Ground>
+                <Player pos={{ x: 0, y: 0.6, z: 0 }} />
+                <Model
+                  meshDir="/glb/jeoungeun/church/church_1.gltf"
+                  meshName="church_1"
+                  mtlName="church_1_mtl"
+                />
+                <Model
+                  meshDir="/glb/jeoungeun/church/church_2.gltf"
+                  meshName="church_2"
+                  mtlName="church_2_mtl"
+                />
+                <Model
+                  meshDir="/glb/jeoungeun/JEOUNGEUN_join.gltf"
+                  meshName="Group"
+                  mtlName="Sculpture"
+                  pos={{ x: 0, y: 0, z: -2 }}
+                  scale={{ x: 3, y: 3, z: 3 }}
+                />
+              </Physics>
+            </WebGPU>
+          </KeyboardControls>
+        </>
       }
     </main>
   );
---
+++ app/projects/test/test/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Model } from "@/app/utils/cli/Model";
+import { WebGPU } from "@/app/utils/cli/WebGPU";
+
+import {
+  Bounds,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+  useGLTF,
+} from "@react-three/drei";
+
+export default function JeongeunTest() {
+  return (
+    <main style={{ height: "100vh", width: "100%" }} className="bg-black">
+      {/*       <Canvas>
+        <OrbitControls makeDefault autoRotate />
+        <PerspectiveCamera position={[2, 1, 2]} makeDefault />
+
+        <Environment preset="city" />
+        <ambientLight intensity={0.5} />
+        <Bounds fit clip observe margin={1.3}>
+          <Thing />
+        </Bounds>
+      </Canvas> */}
+
+      {
+        <WebGPU>
+          <OrbitControls makeDefault />
+          <PerspectiveCamera position={[2, 1, 2]} makeDefault near={0.001} />
+
+          <Environment files="/hdr/hansaplatz_2k.hdr" background />
+          <ambientLight intensity={0.5} />
+
+          <Bounds fit clip margin={3}>
+            <Model
+              meshDir="/glb/jeoungeun/JEOUNGEUN_join.gltf"
+              meshName="Group"
+              mtlName="Sculpture"
+            />
+          </Bounds>
+        </WebGPU>
+      }
+    </main>
+  );
+}
